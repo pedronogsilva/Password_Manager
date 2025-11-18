13973b215e16d4fbd07a0e66340013a3e7645d19
Pedro Silva <pedronsilva100@gmail.com>
2025-11-18 02:17:32 +0000

    Version 0.0.0

---
+++ src/App.java
@@ -0,0 +1,19 @@
+/*
+This code consists of a password manager application that works through a Command Line Interface.
+This project was started on 11/18 at 2:10 AM. It was being developed because I was simply saving my passwords in a .txt file.
+It wasn't organized no matter how hard we tried. With this application, the organization will be much better.
+
+Starting Date: 11/18/2025 - Finish Date: "ongoing"
+
+Version 0.0.0 -> 11/18/2025
+Version 0.0.1 -> 11/XX/2025
+Version 0.0.0 -> 11/XX/2025
+Version 0.0.0 -> 11/XX/2025
+Version 0.0.0 -> 11/XX/2025
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
