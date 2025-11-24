9090025b3abf57b2770986220b77c6627605bf3c
Pedro Silva <pedronsilva100@gmail.com>
2025-11-24 23:08:39 +0000

    Version 0.1.2

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ It wasn't organized no matter how hard we tried. With this application, the orga
 Starting Date: 11/18/2025 - Finish Date: "ongoing"
 
 Version 0.0.0 -> 11/18/2025
-Version 0.0.1 -> 11/22/2025 -> XX/XX/202X (Visual)
-Version 0.0.0 -> XX/XX/202X -> XX/XX/202X
+Version 0.0.1 -> 11/22/2025 -> 11/24/2025
+    Semi-Visual
+Version 0.1.2 -> 11/24/2025 -> XX/XX/202X
+    Visual, Lingual Nova, Criação DB, Adicionar
 Version 0.0.0 -> XX/XX/202X -> XX/XX/202X
 Version 0.0.0 -> XX/XX/202X -> XX/XX/202X
 """
@@ -20,8 +22,9 @@ def initialize_db():
     c = conn.cursor()
     c.execute('''
         CREATE TABLE IF NOT EXISTS passwords (
-            id INTEGER PRIMARY KEY,
-            service TEXT NOT NULL,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            site TEXT NOT NULL,
+            login TEXT NOT NULL,
             username TEXT NOT NULL,
             password TEXT NOT NULL
         )
@@ -29,34 +32,88 @@ def initialize_db():
     conn.commit()
     conn.close()
 
+
 def clear():
     os.system("cls")
 
 
+def add():
+    conn = sqlite3.connect('passwords.db')
+    c = conn.cursor()
 
-
-
-
-
-clear()
-print("---------------PassWord Manager App ---------------\n\n      [1] Add Password\n      [2] View Passwords\n      [3] Edit Password\n      [4] Delete Password\n      [5] Exit App")
-Option = int(input("\n  ->"))
-print(Option)
-
-if Option == 1:
     clear()
-    print("1")
-elif Option == 2:
+    print("---------------Adding Password---------------\n\n      What is the app or website?")
+    site = input("  ->").strip().capitalize()
+
     clear()
-    print("2")
-elif Option == 3:
+    print("---------------Adding Password---------------\n\n      [1] Username / Password\n      [2] Fast Login")
+    login = input("\n  ->").strip()
+
+    if login == "1":
+        clear()
+        print("---------------Adding Password---------------\n\n      Username/Email:")
+        username = input("  ->").strip()
+        print("\n      Password:")
+        password = input("  ->").strip()
+        c.execute("INSERT INTO passwords (site, login, username, password) VALUES (?, ?, ?, ?)", (site, login, username, password))
+        conn.commit()
+
+    elif login == "2":
+        clear()
+        print("---------------Adding Password---------------\n\n      What's the quick login?")
+        quick = input("  ->").strip().capitalize()
+        c.execute("SELECT * FROM passwords WHERE site = ?", (quick,))
+        result = c.fetchall()
+
+        print("---------------Adding Password---------------\n\n")
+
+
+        found_id, found_site, found_login, found_username, found_password = result
+
+        c.execute("INSERT INTO passwords (site, login, username, password) VALUES (?, ?, ?, ?)", (site, login, found_username, found_password))
+        conn.commit()
+
+
+        """if result is None:
+            print("\nQuick login not found in the database!")
+            return
+        """
+
+        
+
+
+    else:
+        print("Invalid Option! Please try again.")
+    
+
+
+
+
+
+
+while True:
+
     clear()
-    print("3")
-elif Option == 4:
-    clear()
-    print("4")
-elif Option == 5:
-    clear()
-    print("5")
-else:
-    print("Invaid Input")
+    initialize_db()
+    print("---------------PassWord Manager App ---------------\n\n      [1] Add Password\n      [2] View Passwords\n      [3] Edit Password\n      [4] Delete Password\n      [5] Exit App")
+    option = int(input("\n  ->"))
+    print(option)
+
+    if option == 1:
+        clear()
+        add()
+    elif option == 2:
+        clear()
+        print("2")
+    elif option == 3:
+        clear()
+        print("3")
+    elif option == 4:
+        clear()
+        print("4")
+    elif option == 5:
+        clear()
+        print("5")
+    else:
+        clear()
+        print("Invalid Option! Please try again.")
